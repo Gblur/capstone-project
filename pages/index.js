b9400cb1c05025de28d344d5989b6c8e28bb444d
Florian Gaebler <floriangaebler87@gmail.com>
2023-04-14 11:04:46 +0200

    revert changes

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,11 @@ import Canvas from "../components/Canvas";
 import Filter from "../components/Canvas/FilterData";
 import "reactflow/dist/style.css";
 import {useState} from "react";
-import Dashboard from "../components/Dashboard";
 
 export default function Home() {
-	const [filter, setFilter] = useState("HTML");
-
-	function handleFilter(filter) {
-		setFilter(filter);
-	}
-
 	return (
 		<main style={{height: `calc(100vh - 250px)`}}>
-			<Dashboard />
+			<h2>Mapname</h2>
 		</main>
 	);
 }
