f1f47792ceb2b6b50b462a7439fa99b49ee010d8
Florian Gaebler <floriangaebler87@gmail.com>
2023-04-14 11:27:17 +0200

    check env

--- pages/api/maps/index.js
+++ pages/api/maps/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 	const data = req.body;
 	switch (req.method) {
 		case "GET":
-			const map = await Map.find({});
+			const map = await Map.find();
 			debugger;
 			return res.status(200).json(map);
 		case "POST":
