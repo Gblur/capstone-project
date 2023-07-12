d1594c0dd54b7ab231cc86e67472f54c3b325346
Florian <floriangaebler87@gmail.com>
2023-07-12 19:22:51 +0200

    test webhook

--- pages/api/auth/github.js
+++ pages/api/auth/github.js
@@ -1,16 +1,15 @@
-import {Octokit} from "@octokit/rest";
+import { Octokit } from "@octokit/rest";
 
 export default async function handler(req, res) {
-	const user = req.query;
-	const octokit = new Octokit({
-		auth: process.env.GITHUB_ADMIN_TOKEN,
-	});
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_ADMIN_TOKEN,
+  });
 
-	switch (req.method) {
-		case "GET":
-			const repos = await octokit.request(`/users/Gblur/repos`);
-			return res.status(200).json(repos);
-		case "POST":
-			return res.status(200).json({status: "Cool"});
-	}
+  switch (req.method) {
+    case "GET":
+      const repos = await octokit.request(`/users/Gblur/repos`);
+      return res.status(200).json(repos);
+    case "POST":
+      return res.status(200).json({ status: "Cool" });
+  }
 }
